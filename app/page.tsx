09a33409de270ec1b6ffb037caa21fc86eee79d2
Anudit Nagar <nagaranudit@gmail.com>
2024-01-20 12:05:21 +0530

    Quick Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Particles from "./components/particles";
 
 export default function Home() {
@@ -20,11 +19,11 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					The only thing that you'll ever need, and don't need to carry around.{" "}
+					A Digital Identity Ecocsytem for the 21<sup>st</sup> Century{" "}
 				</h2>
-				<h2 className="text-sm text-zinc-500 ">
+				{/* <h2 className="text-sm text-zinc-500 ">
 					Shipping soon.
-				</h2>
+				</h2> */}
 			</div>
 		</div>
 	);
